4645e3bef4b4440cf88dc72efdf596c2885acfbd
Kibo9 <krishpatel7708@gmail.com>
2024-09-23 14:24:00 -0400

    sigma balls

--- flappy_update.py
+++ flappy_update.py
@@ -131,21 +131,60 @@ SCOREEVENT = pygame.USEREVENT + 2
 pygame.time.set_timer(SCOREEVENT,100)
 
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			sys.exit()
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_SPACE and game_active:
-				bird_movement = 0
-				bird_movement -= 12
-				flap_sound.play()
-			if event.key == pygame.K_SPACE and game_active == False:
-				game_active = True
-				pipe_list.clear()
-				bird_rect.center = (100,512)
-				bird_movement = 0
-				score = 0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and game_active:
+                bird_movement = 0
+                bird_movement -= 12
+                flap_sound.play()
+            if event.key == pygame.K_SPACE and not game_active:
+                game_active = True
+                pipe_list.clear()
+                bird_rect.center = (100, 512)
+                bird_movement = 0
+                score = 0
+
+		if event.type == SPAWNPIPE:
+        pipe_list.extend(create_pipe())
+
+        if event.type == BIRDFLAP:
+            bird_index = (bird_index + 1) % len(bird_frames)
+            bird_surface, bird_rect = bird_animation()
+
+    screen.blit(bg_surface, (0, 0))
+
+    if game_active:
+        # Bird movement
+        bird_movement += gravity
+        rotated_bird = rotate_bird(bird_surface)
+        bird_rect.centery += bird_movement
+        screen.blit(rotated_bird, bird_rect)
+        game_active = check_collision(pipe_list)
+
+        # Move and draw pipes
+        pipe_list = move_pipes(pipe_list)
+        draw_pipes(pipe_list)
+
+        # Check score
+        pipe_score_check()
+        score_display('main_game')
+    else:
+        screen.blit(game_over_surface, game_over_rect)
+        high_score = update_score(score, high_score)
+        score_display('game_over')
+
+    # Draw floor
+    floor_x_pos -= 1
+    draw_floor()
+    if floor_x_pos <= -576:
+        floor_x_pos = 0
+
+    pygame.display.update()
+    clock.tick(120)
+
 
 		if event.type == SPAWNPIPE:
 			pipe_list.extend(create_pipe())
